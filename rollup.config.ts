59c97d2f44e425cad21d801b39e8fa3ba9cc1a6e
eternal <eternal.git@gmail.com>
2023-04-29 20:25:18 +0100

    output iife to avoid wrapping in module

--- rollup.config.ts
+++ rollup.config.ts
@@ -9,37 +9,37 @@ import replace from '@rollup/plugin-replace';
 import json from '@rollup/plugin-json';
 
 const revision = (() => {
-  try {
-    return execSync('git rev-parse --short HEAD').toString().trim();
-  } catch {
-    return 'N/A';
-  }
+	try {
+		return execSync('git rev-parse --short HEAD').toString().trim();
+	} catch {
+		return 'N/A';
+	}
 })();
 
 const config: RollupOptions = {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/bundle.js',
-      format: 'esm',
-      inlineDynamicImports: true,
-      strict: false
-    }
-  ],
+	input: 'src/index.ts',
+	output: [
+		{
+			file: 'dist/bundle.js',
+			format: 'iife',
+			inlineDynamicImports: true,
+			strict: false
+		}
+	],
 
-  plugins: [
-    paths({ preserveExtensions: true, nonRelative: true }),
-    node(),
-    json(),
-    replace({ preventAssignment: true, __VERSION__: revision }),
-    swc(),
-    minify({ compress: true, mangle: true })
-  ],
+	plugins: [
+		paths({ preserveExtensions: true, nonRelative: true }),
+		node(),
+		json(),
+		replace({ preventAssignment: true, __VERSION__: revision }),
+		swc(),
+		minify({ compress: true, mangle: true })
+	],
 
-  onwarn(warning, warn) {
-    if (warning.code === 'EVAL') return;
-    warn(warning);
-  }
+	onwarn(warning, warn) {
+		if (warning.code === 'EVAL') return;
+		warn(warning);
+	}
 };
 
 export default config;
