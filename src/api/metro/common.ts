1184085363fa2a1e94707f2d9d74c708ee969afe
Acquite <acquitefx@gmail.com>
2023-06-04 19:05:47 +0100

    Fix Constants metro module find

--- src/api/metro/common.ts
+++ src/api/metro/common.ts
@@ -13,7 +13,7 @@ export const React: Common.React = window.React;
 // Discord
 export const StyleSheet: Common.StyleSheets = findByProps('createThemedStyleSheet', { lazy: true });
 export const Dispatcher: Common.Dispatcher = findByProps('_dispatch', { lazy: true });
-export const Constants = findByProps('API_HOST', 'Endpoints', { lazy: true });
+export const Constants = findByProps('Fonts', 'Endpoints', { lazy: true });
 export const Theme = findByProps('colors', 'meta', { lazy: true });
 export const REST = findByProps('getAPIBaseURL', { lazy: true });
 export const i18n = findByProps('Messages', { lazy: true });
