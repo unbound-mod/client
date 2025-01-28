bc219c3f6d83346b717231ea61b228117a4f469f
Mario <me@marioparaschiv.com>
2025-01-28 09:25:11 +0000

    🎨 fmt: imports

--- src/built-ins/connecting-fix.ts
+++ src/built-ins/connecting-fix.ts
@@ -1,7 +1,8 @@
 import type { BuiltInData } from '@typings/built-ins';
 import { findByProps, findStore } from '@api/metro';
-import { createPatcher } from '@patcher';
 import { Dispatcher } from '@api/metro/common';
+import { createPatcher } from '@patcher';
+
 
 const Patcher = createPatcher('unbound::connecting_fix');
 
@@ -10,20 +11,20 @@ export const data: BuiltInData = {
 };
 
 export function start() {
-  const AuthenticationUtilities = findByProps('startSession', { lazy: true });
-  const AuthenticationStore = findStore('Authentication');
+	const AuthenticationUtilities = findByProps('startSession', { lazy: true });
+	const AuthenticationStore = findStore('Authentication');
 
-  Patcher.after(AuthenticationUtilities, 'startSession', () => {
-      setTimeout(() => {
-          if (!AuthenticationStore.getSessionId()) {
-              // Borrowed from @octet-stream <@263530950070239235>
-              // Reference image https://i.imgur.com/P5Rk5hu.png
-              Dispatcher.dispatch({ type: 'APP_STATE_UPDATE', state: 'inactive' });
-              Dispatcher.dispatch({ type: 'APP_STATE_UPDATE', state: 'background' });
-              Dispatcher.dispatch({ type: 'APP_STATE_UPDATE', state: 'active' });
-          }
-      }, 300)
-  }, true);
+	Patcher.after(AuthenticationUtilities, 'startSession', () => {
+		setTimeout(() => {
+			if (!AuthenticationStore.getSessionId()) {
+				// Borrowed from @octet-stream <@263530950070239235>
+				// Reference image https://i.imgur.com/P5Rk5hu.png
+				Dispatcher.dispatch({ type: 'APP_STATE_UPDATE', state: 'inactive' });
+				Dispatcher.dispatch({ type: 'APP_STATE_UPDATE', state: 'background' });
+				Dispatcher.dispatch({ type: 'APP_STATE_UPDATE', state: 'active' });
+			}
+		}, 300);
+	}, true);
 }
 
 export function stop() {
