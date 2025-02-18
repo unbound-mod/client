b29c349930d36c1e450c42e118a7bee6fd9ead3b
Mario P <me@marioparaschiv.com>
2025-02-18 16:16:56 +0000

    🧑‍💻 dev: show local address if present

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,26 @@
+import os from 'node:os';
+
+
 console.clear();
 
 const BASE_PATH = './dist';
 const PORT = Bun.argv[2] ?? 8080;
 
 const getCurrentDate = () => new Date().toTimeString().split(' ')[0];
+const getLocalIp = () => {
+	const interfaces = os.networkInterfaces();
+
+	for (const ifaces of Object.values(interfaces)) {
+		if (!ifaces) continue;
+
+		for (const iface of ifaces) {
+			// Skip over internal (i.e., 127.0.0.1) and non-IPv4 addresses
+			if (iface.family === 'IPv4' && !iface.internal) {
+				return iface.address;
+			}
+		}
+	}
+};
 
 Bun.serve({
 	port: PORT,
@@ -28,4 +45,8 @@ Bun.serve({
 	},
 });
 
-console.log(`Listening to port ${PORT}. The time is ${getCurrentDate()}.`);
+if (getLocalIp()) {
+	console.log(`Listening on ${getLocalIp()}:${PORT}. The time is ${getCurrentDate()}.`);
+} else {
+	console.log(`Listening on port ${PORT}. The time is ${getCurrentDate()}.`);
+}
